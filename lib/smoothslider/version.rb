5c599bebe29fb1076d983eb29270f9db64991c64
Arjun Ghimire <arjunghimire0714@gmail.com>
2018-01-21 14:20:27 +0545

    update version and update

--- lib/smoothslider/version.rb
+++ lib/smoothslider/version.rb
@@ -1,3 +1,3 @@
 module Smoothslider
-  VERSION = "1.1.5"
+  VERSION = "2.0"
 end
--- vendor/assets/javascripts/smoothslider.js
+++ vendor/assets/javascripts/smoothslider.js
@@ -1597,3 +1597,11 @@ $(function(){
   });
   toggles.first().click();
 });
+
+
+jQuery(document).ready(function($) {
+  $(".scroll").click(function(event){   
+  event.preventDefault();
+  $('html,body').animate({scrollTop:$(this.hash).offset().top},1000);
+});
+});
